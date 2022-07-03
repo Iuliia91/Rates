4d574c61fc2c1af53b0ae81d6bc34ff82bc701fc
Your Name <you@example.com>
2022-07-03 09:36:27 +0300

    comit

--- src/HOC/GlobalModalProvider.jsx
+++ src/HOC/GlobalModalProvider.jsx
@@ -38,19 +38,11 @@ const StyledGlobalModalProvider = styled.div`
       }
     }
   }
-
-  @media screen and (max-width: 800px) {
-    .modal {
-      max-width: 600px;
-      width: 100%;
-      padding: 0;
-    }
-  }
 `
 
 const GlobalModalProvider = (props) => {
   const [modalContext, setModalContext] = useState()
-
+  console.log(modalContext)
   return (
     <React.Fragment>
       {!!modalContext && (
